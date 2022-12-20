b122605ae1cb26eda90b26b0026b4884ab22e9d6
reiken <ultee529@outlook.jp>
2022-12-20 17:04:41 +0900

    PracticeAWelcomeToAtCoder AC

--- PracticeAWelcomeToAtCoder/src/Main.java
+++ PracticeAWelcomeToAtCoder/src/Main.java
@@ -1,10 +1,23 @@
-
+import java.util.Scanner;
 
 public class Main {
-
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();    
+    int c = sc.nextInt();
+    String s = sc.next();
+
+    System.out.print(a+b+c);
+    System.out.print(" ");
+    System.out.print(s);
+
+    
+    
+    
 
   }
-
 }
+  
